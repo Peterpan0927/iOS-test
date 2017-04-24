8345937dfda8ad17c398341246ee7756c9241edc
Peterpan0927 <1549346936@qq.com>
2017-04-24 15:57:55 +0800

    plus

--- PhotoViewer/PhotoViewer/ViewController.m
+++ PhotoViewer/PhotoViewer/ViewController.m
@@ -15,6 +15,7 @@
 @property (weak, nonatomic) IBOutlet UIButton *nextbutton;
 @property (weak, nonatomic) IBOutlet UILabel *indexLabel;
 @property (weak, nonatomic) IBOutlet UILabel *infoLabel;
+@property (strong, nonatomic) NSArray *pictures;
 
 
 @end
@@ -24,6 +25,10 @@
 - (void)viewDidLoad {
     //当控制器的view和上面的子控件创建完毕之后就会执行这个方法
     [super viewDidLoad];
+    NSDictionary *dict1 = @{@"icon":@"1.jpeg",@"info":@"1/3",@"index":@"唔哈哈哈哈哈哈哈"};
+    NSDictionary *dict2 = @{@"icon":@"2.jpg",@"info":@"2/3",@"index":@"小朋友们还记得我吗？我就是光头"};
+    NSDictionary *dict3 = @{@"icon":@"3.jpg",@"info":@"3/3",@"index":@"紫薇，站住不许走"};
+    self.pictures = @[dict1, dict2, dict3];
     self.index = 1;
     self.lastbutton.enabled = NO;
     [self changeImage];
@@ -32,38 +37,19 @@
 - (IBAction)LastButton:(UIButton *)sender {
     self.index--;
     [self changeImage];
-    if (self.index==1)
-        sender.enabled = NO;
-    if (self.index!=1)
-        self.nextbutton.enabled = YES;
 }
 - (IBAction)NextButton:(UIButton *)sender {
     self.index++;
     [self changeImage];
-    if (self.index==3)
-        sender.enabled = NO;
-    if (self.index!=3)
-        self.lastbutton.enabled = YES;
-}
+   }
 
 - (void)changeImage{
-    switch (self.index) {
-        case 1:
-            self.indexLabel.text = @"唔哈哈哈哈";
-            self.infoLabel.text = @"1/3";
-            self.imageview.image = [UIImage imageNamed:@"1.jpeg"];
-            break;
-        case 2:
-            self.indexLabel.text = @"小朋友们，还记得我吗？";
-            self.infoLabel.text = @"2/3";
-            self.imageview.image = [UIImage imageNamed:@"2.jpg"];
-            break;
-        case 3:
-            self.indexLabel.text = @"站住不许走";
-            self.infoLabel.text = @"3/3";
-            self.imageview.image = [UIImage imageNamed:@"3.jpg"];
-            break;
-            }
-}
+    NSDictionary *dict = self.pictures[self.index-1];
+    self.infoLabel.text = dict[@"info"];
+    self.indexLabel.text = dict[@"index"];
+    self.imageview.image = [UIImage imageNamed:dict[@"icon"]];
+    self.lastbutton.enabled = (self.index==1)?NO:YES;
+    self.nextbutton.enabled = (self.index==3)?NO:YES;
+    }
 
 @end
